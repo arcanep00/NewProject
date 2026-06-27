ef7f715b765cf07ea704dfc2a404fef4ceb38de6
Abhinav Pandey <navabhiday00@gmail.com>
2026-06-27 13:09:54 +0530

    Configure environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,13 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    APP_NAME: str = "Backend Devops Assignment API"
-    APP_VERSION: str = "1.0.0"
-    DEBUG: bool = True
+    APP_NAME: str
+    APP_VERSION: str
+    DEBUG: bool
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
 
 settings = Settings()
