d1bb34607827bd3559ca5e8c36d1ff4541267786
Abhinav Pandey <navabhiday00@gmail.com>
2026-06-27 16:04:15 +0530

    Create initial Alembic migration

--- app/db/init__db.py
+++
@@ -1,5 +0,0 @@
-from app.db.database import engine
-from app.db.base import Base
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
 from app.core.config import settings
-from app.db.init__db import init_db
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION
 )
-
-@app.on_event("startup")
-def startup():
-    init_db()
 
 @app.get("/")
 def root():
